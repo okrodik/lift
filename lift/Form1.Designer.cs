c94d7f0db51527f01e5cbf92d012818f7f9ded81
Аина <okrodik@gmail.com>
2025-11-28 01:15:35 +1100

    Готовый проект

--- lift/Form1.Designer.cs
+++ lift/Form1.Designer.cs
@@ -29,20 +29,17 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.knopka = new System.Windows.Forms.Panel();
             this.etaz = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.knopka = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.etaz);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.knopka);
@@ -52,15 +49,12 @@
             this.panel1.Size = new System.Drawing.Size(800, 450);
             this.panel1.TabIndex = 0;
             // 
-            // pictureBox1
+            // etaz
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(274, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(268, 426);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.etaz.Location = new System.Drawing.Point(12, 12);
+            this.etaz.Name = "etaz";
+            this.etaz.Size = new System.Drawing.Size(256, 426);
+            this.etaz.TabIndex = 0;
             // 
             // knopka
             // 
@@ -69,26 +63,19 @@
             this.knopka.Size = new System.Drawing.Size(148, 143);
             this.knopka.TabIndex = 0;
             // 
-            // etaz
-            // 
-            this.etaz.Location = new System.Drawing.Point(12, 12);
-            this.etaz.Name = "etaz";
-            this.etaz.Size = new System.Drawing.Size(256, 426);
-            this.etaz.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(548, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            // 
             // timer1
             // 
             this.timer1.Interval = 1000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::lift.Properties.Resources.openDoor;
+            this.pictureBox1.Location = new System.Drawing.Point(274, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(268, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
@@ -99,7 +86,6 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -111,7 +97,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel knopka;
         private System.Windows.Forms.Panel etaz;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
     }
 }
--- lift/Form1.cs
+++ lift/Form1.cs
@@ -16,132 +16,187 @@ namespace lift
         Button[] buttons_strelki;
         Button[] knopki;
 
-        int elevator;
+        int oldNomer;
+        int newNomer;
+        bool isMoving;
         public Form1()
         {
             InitializeComponent();
             createButtonPanelEtaz();
+
+            oldNomer = 0;
+            newNomer = 0;
+            isMoving = false;
         }
 
         private void createButtonPanelEtaz()
         {
             int x = 6;
             buttons = new Button[6];
-            buttons_strelki = new Button[12];
+            buttons_strelki = new Button[10];
             knopki = new Button[6];
+
+            int strelkaIndex = 0;
+
             for (int i = 0; i < 6; i++)
             {
                 buttons[i] = new Button();
                 buttons[i].Size = new Size(50, 50);
                 buttons[i].Location = new Point(25, 50 + 55 * (i));
-                buttons[i].Click += Button_Click;
                 buttons[i].Text = x.ToString();
                 buttons[i].Enabled = false;
-                buttons[i].FlatAppearance.BorderColor = Color.Yellow;
                 buttons[i].FlatAppearance.BorderSize = 2;
+                buttons[i].FlatAppearance.BorderColor = Color.Yellow;
+                buttons[i].BackColor = Color.White;
                 x--;
                 etaz.Controls.Add(buttons[i]);
 
                 if (i == 0)
                 {
-                    buttons_strelki[1] = new Button();
-                    buttons_strelki[1].Size = new Size(25, 20);
-                    buttons_strelki[1].Location = new Point(buttons[0].Right + 5, buttons[0].Top + 25);
-                    buttons_strelki[1].Click += Vizov_Lift;
-                    buttons_strelki[1].Text = "↓";
-                    buttons_strelki[1].Tag = i.ToString();
-                    etaz.Controls.Add(buttons_strelki[1]);
+                    buttons_strelki[strelkaIndex] = new Button();
+                    buttons_strelki[strelkaIndex].Size = new Size(25, 20);
+                    buttons_strelki[strelkaIndex].Location = new Point(buttons[0].Right + 5, buttons[0].Top + 25);
+                    buttons_strelki[strelkaIndex].Click += Vizov_Lift_Vniz;
+                    buttons_strelki[strelkaIndex].Text = "↓";
+                    buttons_strelki[strelkaIndex].Tag = i.ToString();
+                    etaz.Controls.Add(buttons_strelki[strelkaIndex]);
+                    strelkaIndex++;
                 }
                 else if (i == 5)
                 {
-                    buttons_strelki[10] = new Button();
-                    buttons_strelki[10].Size = new Size(25, 20);
-                    buttons_strelki[10].Location = new Point(buttons[5].Right + 5, buttons[5].Bottom - 45);
-                    buttons_strelki[10].Click += Vizov_Lift;
-                    buttons_strelki[10].Text = "↑";
-                    buttons_strelki[10].Tag = i.ToString();
-                    etaz.Controls.Add(buttons_strelki[10]);
+                    buttons_strelki[strelkaIndex] = new Button();
+                    buttons_strelki[strelkaIndex].Size = new Size(25, 20);
+                    buttons_strelki[strelkaIndex].Location = new Point(buttons[5].Right + 5, buttons[5].Bottom - 45);
+                    buttons_strelki[strelkaIndex].Click += Vizov_Lift_Verh;
+                    buttons_strelki[strelkaIndex].Text = "↑";
+                    buttons_strelki[strelkaIndex].Tag = i.ToString();
+                    etaz.Controls.Add(buttons_strelki[strelkaIndex]);
+                    strelkaIndex++;
                 }
                 else
                 {
-                    buttons_strelki[i * 2] = new Button();
-                    buttons_strelki[i * 2].Size = new Size(25, 20);
-                    buttons_strelki[i * 2].Location = new Point(buttons[i].Right + 5, buttons[i].Top + 5);
-                    buttons_strelki[i * 2].Click += Vizov_Lift;
-                    buttons_strelki[i * 2].Text = "↑";
-                    buttons_strelki[i * 2].Tag = i.ToString();
-                    etaz.Controls.Add(buttons_strelki[i * 2]);
+                    buttons_strelki[strelkaIndex] = new Button();
+                    buttons_strelki[strelkaIndex].Size = new Size(25, 20);
+                    buttons_strelki[strelkaIndex].Location = new Point(buttons[i].Right + 5, buttons[i].Top + 5);
+                    buttons_strelki[strelkaIndex].Click += Vizov_Lift_Verh;
+                    buttons_strelki[strelkaIndex].Text = "↑";
+                    buttons_strelki[strelkaIndex].Tag = i.ToString();
+                    etaz.Controls.Add(buttons_strelki[strelkaIndex]);
+                    strelkaIndex++;
 
-
-                    buttons_strelki[(i * 2) + 1] = new Button();
-                    buttons_strelki[(i * 2) + 1].Size = new Size(25, 20);
-                    buttons_strelki[(i * 2) + 1].Location = new Point(buttons[i].Right + 5, buttons[i].Bottom - 25);
-                    buttons_strelki[(i * 2) + 1].Click += Vizov_Lift;
-                    buttons_strelki[(i * 2) + 1].Text = "↓";
-                    buttons_strelki[(i * 2) + 1].Tag = i.ToString();
-                    etaz.Controls.Add(buttons_strelki[(i * 2) + 1]);
+                    buttons_strelki[strelkaIndex] = new Button();
+                    buttons_strelki[strelkaIndex].Size = new Size(25, 20);
+                    buttons_strelki[strelkaIndex].Location = new Point(buttons[i].Right + 5, buttons[i].Bottom - 25);
+                    buttons_strelki[strelkaIndex].Click += Vizov_Lift_Vniz;
+                    buttons_strelki[strelkaIndex].Text = "↓";
+                    buttons_strelki[strelkaIndex].Tag = i.ToString();
+                    etaz.Controls.Add(buttons_strelki[strelkaIndex]);
+                    strelkaIndex++;
                 }
             }
-            int y = 6;
+
+            int[] nashiChisla = { 6, 5, 4, 3, 2, 1 };
+
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 2; j++)
                 {
-                    knopki[i] = new Button();
-                    knopki[i].Size = new Size(25, 25);
-                    knopki[i].Location = new Point(55 - 30 * j, 25 + 30 * i);
-                    knopki[i].Click += Lift_Knopki;
-                    knopki[i].Text = (y).ToString();
-                    knopki[i].Tag = y.ToString();
-                    knopka.Controls.Add(knopki[i]);
-                    y--;
+                    int index = i * 2 + j;
+                    int chislo = nashiChisla[index];
+                    knopki[index] = new Button();
+                    knopki[index].Size = new Size(25, 25);
+                    knopki[index].Location = new Point(55 - 30 * j, 25 + 30 * i);
+                    knopki[index].Click += Lift_Knopki;
+                    knopki[index].Text = chislo.ToString();
+                    knopki[index].Tag = (6 - chislo).ToString(); 
+                    knopka.Controls.Add(knopki[index]);
                 }
             }
         }
 
-        private void Button_Click(object sender, EventArgs e)
+        private void Vizov_Lift_Verh(object sender, EventArgs e)
         {
+            Button clickedButton = (Button)sender;
 
+            int buttonNomer = Convert.ToInt32((string)clickedButton.Tag);
+
+            Buttons_Etaz_Kakoy(buttonNomer);
+  
+  
         }
-
-        private void Vizov_Lift(object sender, EventArgs e)
+        private void Vizov_Lift_Vniz(object sender, EventArgs e)
         {
-            //Button clickedButton = (Button)sender;
-            //int buttonNomer = Convert.ToInt32((string)clickedButton.Tag);
-            //Buttons_Etaz_Kakoy(buttonNomer);    
+            Button clickedButton = (Button)sender;
+
+            int buttonNomer = Convert.ToInt32((string)clickedButton.Tag);
+    
+            Buttons_Etaz_Kakoy(buttonNomer);
+
+            
         }
 
         private void Lift_Knopki(object sender, EventArgs e)
         {
             Button clickedButton = (Button)sender;
             int buttonNomer = Convert.ToInt32((string)clickedButton.Tag);
-            Buttons_Etaz_Kakoy(buttonNomer);
 
             
+
+            Buttons_Etaz_Kakoy(buttonNomer);         
         }
 
         private void Buttons_Etaz_Kakoy(int x)
         {
+            if (isMoving)
+            {
+                MessageBox.Show("Лифт уже движется");
+                return;
+            }
+
+            newNomer = x;
+            MethodAsync();
+        }
+
+
+        public async Task MethodAsync()
+        {
+            isMoving = true;
+            pictureBox1.Image = Properties.Resources.closeDoor;
+            int step;
             for (int i = 0; i < 6; i++)
             {
                 buttons[i].BackColor = Color.White;
             }
 
-            buttons[x].BackColor = Color.Red;
+            if (oldNomer > newNomer)
+            {
+                step = -1;
+            }
+            else
+            {
+                step = 1;
+            }
 
-            label1.Text = elevator.ToString();
+            buttons[oldNomer].BackColor = Color.Red;
 
-            timer1.Start();
-            elevator = x;
-            timer1.Stop();
+            for (int i = oldNomer; i != newNomer; i += step)
+            {
+                await Task.Delay(1000);
 
+                int currentFloor = i + step;
 
-        }
+                buttons[i].BackColor = Color.White;
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            label1.Text = @"Лифт находится в {x} этаже";
-            label1.Text = elevator.ToString();
+                buttons[currentFloor].BackColor = Color.Red;
+
+                oldNomer = currentFloor; 
+            }
+
+            buttons[newNomer].BackColor = Color.Green;
+            pictureBox1.Image = Properties.Resources.openDoor;
+            oldNomer = newNomer;
+
+            isMoving = false;
         }
     }
 }
