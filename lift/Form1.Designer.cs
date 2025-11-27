5deeb6e78427f34398db5b1d6668fddbab845cb6
poguk12 <okrodik@gmail.com>
2025-11-27 16:42:53 +0900

    Добавьте файлы проекта.

---
+++ lift/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace lift
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.knopka = new System.Windows.Forms.Panel();
+            this.etaz = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.etaz);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.knopka);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(274, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(268, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // knopka
+            // 
+            this.knopka.Location = new System.Drawing.Point(548, 113);
+            this.knopka.Name = "knopka";
+            this.knopka.Size = new System.Drawing.Size(148, 143);
+            this.knopka.TabIndex = 0;
+            // 
+            // etaz
+            // 
+            this.etaz.Location = new System.Drawing.Point(12, 12);
+            this.etaz.Name = "etaz";
+            this.etaz.Size = new System.Drawing.Size(256, 426);
+            this.etaz.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(548, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel knopka;
+        private System.Windows.Forms.Panel etaz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ lift/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lift
+{
+    public partial class Form1 : Form
+    {
+        Button[] buttons;
+        Button[] buttons_strelki;
+        Button[] knopki;
+
+        int elevator;
+        public Form1()
+        {
+            InitializeComponent();
+            createButtonPanelEtaz();
+        }
+
+        private void createButtonPanelEtaz()
+        {
+            int x = 6;
+            buttons = new Button[6];
+            buttons_strelki = new Button[12];
+            knopki = new Button[6];
+            for (int i = 0; i < 6; i++)
+            {
+                buttons[i] = new Button();
+                buttons[i].Size = new Size(50, 50);
+                buttons[i].Location = new Point(25, 50 + 55 * (i));
+                buttons[i].Click += Button_Click;
+                buttons[i].Text = x.ToString();
+                buttons[i].Enabled = false;
+                buttons[i].FlatAppearance.BorderColor = Color.Yellow;
+                buttons[i].FlatAppearance.BorderSize = 2;
+                x--;
+                etaz.Controls.Add(buttons[i]);
+
+                if (i == 0)
+                {
+                    buttons_strelki[1] = new Button();
+                    buttons_strelki[1].Size = new Size(25, 20);
+                    buttons_strelki[1].Location = new Point(buttons[0].Right + 5, buttons[0].Top + 25);
+                    buttons_strelki[1].Click += Vizov_Lift;
+                    buttons_strelki[1].Text = "↓";
+                    buttons_strelki[1].Tag = i.ToString();
+                    etaz.Controls.Add(buttons_strelki[1]);
+                }
+                else if (i == 5)
+                {
+                    buttons_strelki[10] = new Button();
+                    buttons_strelki[10].Size = new Size(25, 20);
+                    buttons_strelki[10].Location = new Point(buttons[5].Right + 5, buttons[5].Bottom - 45);
+                    buttons_strelki[10].Click += Vizov_Lift;
+                    buttons_strelki[10].Text = "↑";
+                    buttons_strelki[10].Tag = i.ToString();
+                    etaz.Controls.Add(buttons_strelki[10]);
+                }
+                else
+                {
+                    buttons_strelki[i * 2] = new Button();
+                    buttons_strelki[i * 2].Size = new Size(25, 20);
+                    buttons_strelki[i * 2].Location = new Point(buttons[i].Right + 5, buttons[i].Top + 5);
+                    buttons_strelki[i * 2].Click += Vizov_Lift;
+                    buttons_strelki[i * 2].Text = "↑";
+                    buttons_strelki[i * 2].Tag = i.ToString();
+                    etaz.Controls.Add(buttons_strelki[i * 2]);
+
+
+                    buttons_strelki[(i * 2) + 1] = new Button();
+                    buttons_strelki[(i * 2) + 1].Size = new Size(25, 20);
+                    buttons_strelki[(i * 2) + 1].Location = new Point(buttons[i].Right + 5, buttons[i].Bottom - 25);
+                    buttons_strelki[(i * 2) + 1].Click += Vizov_Lift;
+                    buttons_strelki[(i * 2) + 1].Text = "↓";
+                    buttons_strelki[(i * 2) + 1].Tag = i.ToString();
+                    etaz.Controls.Add(buttons_strelki[(i * 2) + 1]);
+                }
+            }
+            int y = 6;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    knopki[i] = new Button();
+                    knopki[i].Size = new Size(25, 25);
+                    knopki[i].Location = new Point(55 - 30 * j, 25 + 30 * i);
+                    knopki[i].Click += Lift_Knopki;
+                    knopki[i].Text = (y).ToString();
+                    knopki[i].Tag = y.ToString();
+                    knopka.Controls.Add(knopki[i]);
+                    y--;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Vizov_Lift(object sender, EventArgs e)
+        {
+            //Button clickedButton = (Button)sender;
+            //int buttonNomer = Convert.ToInt32((string)clickedButton.Tag);
+            //Buttons_Etaz_Kakoy(buttonNomer);    
+        }
+
+        private void Lift_Knopki(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            int buttonNomer = Convert.ToInt32((string)clickedButton.Tag);
+            Buttons_Etaz_Kakoy(buttonNomer);
+
+            
+        }
+
+        private void Buttons_Etaz_Kakoy(int x)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                buttons[i].BackColor = Color.White;
+            }
+
+            buttons[x].BackColor = Color.Red;
+
+            label1.Text = elevator.ToString();
+
+            timer1.Start();
+            elevator = x;
+            timer1.Stop();
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = @"Лифт находится в {x} этаже";
+            label1.Text = elevator.ToString();
+        }
+    }
+}
